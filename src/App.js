3f223030e896861623147642836a088153d30a47
runhelp <runhelp@mail.ru>
2019-03-23 21:56:05 +0300

    21:55

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import connect from '@vkontakte/vkui-connect';
 import { View } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
+import fetchJsonp from 'fetch-jsonp';
 import Home from './panels/Home';
-
 
 class App extends React.Component {
 	constructor(props) {
@@ -12,7 +12,11 @@ class App extends React.Component {
 		this.state = {
 			activePanel: 'home',
 			fetchedUser: null,
+			authToken : null,
+			items : []
 		};
+
+		this.getItems = this.getItems.bind(this)
 	}
 
 	componentDidMount() {
@@ -21,23 +25,35 @@ class App extends React.Component {
 				case 'VKWebAppGetUserInfoResult':
 					this.setState({ fetchedUser: e.detail.data });
 					break;
+				case 'VKWebAppAccessTokenReceived':
+					this.setState({ authToken : e.detail.data.access_token });
+					this.getItems()
+					break;
 				default:
 					console.log(e.detail.type);
 			}
 		});
 		connect.send('VKWebAppGetUserInfo', {});
-		
+		connect.send("VKWebAppGetAuthToken", {"app_id": 6906999, "scope": "market"});
 	}
 
 	go = (e) => {
 		this.setState({ activePanel: e.currentTarget.dataset.to })
 	};
 
+	getItems() {
+		const ownerId = 124527492
+		let api = `https://api.vk.com/method/market.get?v=5.52&access_token=${this.state.authToken}&owner_id=-${ownerId}`
+		fetchJsonp(api)
+		.then(res => res.json())
+		.then(data => this.setState({ items : data.response.items}))
+		.catch(e => [])
+	}
+
 	render() {
 		return (
 			<View activePanel={this.state.activePanel}>
-				<Home id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
-
+				<Home id="home" items={this.state.items} fetchedUser={this.state.fetchedUser} go={this.go} />
 			</View>
 		);
 	}
