423b8f2f77683c33ba5c4a1f98e1bd6ae5a951bc
runhelp <runhelp@mail.ru>
2019-03-23 22:05:04 +0300

    Updates

---
+++ precache-manifest.c50995d5a6fbc196702bf38bace16232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4188da6b21417c0d6b",
+    "url": "/ecomerce-app/static/js/runtime~main.9d4188da.js"
+  },
+  {
+    "revision": "fd95c9731c7ff586f1ba",
+    "url": "/ecomerce-app/static/js/main.fd95c973.chunk.js"
+  },
+  {
+    "revision": "b3c385e920acffac43fc",
+    "url": "/ecomerce-app/static/js/2.b3c385e9.chunk.js"
+  },
+  {
+    "revision": "b3c385e920acffac43fc",
+    "url": "/ecomerce-app/static/css/2.5047dbea.chunk.css"
+  },
+  {
+    "revision": "37aac80a0703fceaadb6020649d6f4ca",
+    "url": "/ecomerce-app/index.html"
+  }
+];
